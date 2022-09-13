18e6e9c5466119e450245c677b07c8c49daf35be
[sead] Samir <samirbadarou@sead.pb.gov.br>
2022-09-12 22:50:50 -0300

    date select

--- backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
+++ backend/src/main/java/com/devsuperior/dsmeta/services/SaleService.java
@@ -25,7 +25,10 @@ public class SaleService {
 	
 	public Page<Sale> findSales(String minDate, String maxDate, Pageable pageable) {
 		
-		LocalDate today = LocalDate.ofInstant(Instant.now(null), ZoneId.systemDefault());
+	LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+		
+		//LocalDate min = LocalDate.parse(minDate);
+		//LocalDate max = LocalDate.parse(maxDate);
 		
 		LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
 		LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
